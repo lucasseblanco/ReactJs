6155a780c64d1403ecbb9566d5520829a47d355f
lucasseblanco <69616137+lucasseblanco@users.noreply.github.com>
2022-04-20 18:01:15 -0300

    Update de ItemListContainer.js / ItemCount.js

---
+++ lucasblanco-project/src/Components/ItemCount.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+const ItemCount = () => {
+    const [contador, setContador] = useState(0)
+    const [stock, setStock] = useState(5)
+
+    const handleClick = () => {
+      setContador(contador + 1)
+      setStock(stock-1)
+      }
+
+      const handleClickRest = () => {
+        setContador(contador - 1)
+        setStock(stock+1)
+      }
+
+      console.log(stock)
+
+      //if (contador <= stock) {
+        //handleClick()
+      //}
+        //else{
+          //alert('No mas stock')
+        //}
+
+
+  if (stock != 0) {
+  return (
+    <div>
+      <button onClick={handleClick} >click</button>
+      <p>contador actual  {contador}</p>
+      <button onClick={handleClickRest} >click</button>
+    </div>
+  )
+}
+else{
+  return(
+  <div>NO HAY MAS STOCK</div>
+  )
+}
+}
+export default ItemCount
--- lucasblanco-project/src/Components/ItemListContainer.js
+++ lucasblanco-project/src/Components/ItemListContainer.js
@@ -1,9 +1,13 @@
+import ItemCount from './ItemCount'
+
+
 const ItemListContainer = () => {
-    const greeting = 'Segunda entrega, espero que esto este bien :D'
+    
 
   return (
-      
-    <div>{greeting}</div>
+      <div>
+        <ItemCount></ItemCount>
+      </div>
   )
 }
 export default ItemListContainer
