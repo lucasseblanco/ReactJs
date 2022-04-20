e08d3f41ab4a1b3de0250ab45df90bf1f0cc8386
lucasseblanco <69616137+lucasseblanco@users.noreply.github.com>
2022-04-19 22:18:49 -0300

    Añadiendo CartWidger y ItemListContainer.js

---
+++ lucasblanco-project/src/Components/ItemListContainer.js
@@ -0,0 +1,9 @@
+const ItemListContainer = () => {
+    const greeting = 'Segunda entrega, espero que esto este bien :D'
+
+  return (
+      
+    <div>{greeting}</div>
+  )
+}
+export default ItemListContainer
