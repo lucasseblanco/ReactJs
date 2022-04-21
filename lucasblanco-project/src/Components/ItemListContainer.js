f980363c2438ed0f6e6ed7c28941be743a017aa9
lucasseblanco <69616137+lucasseblanco@users.noreply.github.com>
2022-04-21 15:13:51 -0300

    Update ItemListContainer.js

--- lucasblanco-project/src/Components/ItemListContainer.js
+++ lucasblanco-project/src/Components/ItemListContainer.js
@@ -1,9 +1,18 @@
-const ItemListContainer = () => {
+import ItemCount from './ItemCount'
+import ItemList from './ItemList'
+
     const greeting = 'Segunda entrega, espero que esto este bien :D'
 
+
+const ItemListContainer = () => {
+    
+
   return (
-      
-    <div>{greeting}</div>
+      <div>
+        <ItemCount/>
+          {greeting}
+        <ItemList/>
+      </div>
   )
 }
 export default ItemListContainer
